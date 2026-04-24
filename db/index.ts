8d21a01646e03edba0c528d9eefa03416c5a6764
Bagus Priambudi <bagusnetagain@gmail.com>
2026-04-24 22:48:16 +0700

    chore: switch drizzle driver to postgres-js for Prisma compatibility

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,9 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema });
+const connectionString = process.env.POSTGRES_URL || "";
+
+// Disable prefetch as some providers like Prisma/Supabase pooling don't support it well
+export const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client, { schema });
